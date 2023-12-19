63894abd738942c6dbefca765f004ffd83d9e7ae
Sanduranga <edirisinghest527@gmail.com>
2023-12-20 02:51:49 +0530

    upgraded

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
-const Button = () => {
+import PropTypes from "prop-types";
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const Button = (props) => {
   return (
     <div className="px-5 py-3 bg-orange-400 rounded justify-start items-start gap-2 inline-flex">
       <h3 className="text-white text-sm font-bold font-['Inter'] uppercase leading-[14px]">
-        Get free consultation
+        {props.children}
       </h3>
     </div>
   );
