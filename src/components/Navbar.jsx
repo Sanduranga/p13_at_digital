5f59a630924489bf68bdfe8bb438a4aacd40a4ec
Sanduranga <edirisinghest527@gmail.com>
2023-12-25 14:53:17 +0530

    upgraded Navigation bar enabling mobile navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,67 @@
+import { useEffect, useRef, useState } from "react";
 import { logo, menuIcon } from "../assets";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 const Navbar = () => {
+  const [mobileNavBtn, setmobileNavBtn] = useState(false);
+  const catchScreen = useRef(null);
+
+  useEffect(() => {
+    const contentElement = catchScreen.current;
+
+    if (mobileNavBtn) {
+      disableBodyScroll(contentElement);
+    } else {
+      enableBodyScroll(contentElement);
+    }
+    // Enable scroll on the body when the component will unmounts*********************
+    return () => {
+      enableBodyScroll(contentElement);
+    };
+  }, [mobileNavBtn]);
+
   return (
-    <nav className="fixed w-full flex justify-between items-center min-w-[250px] max-w-full mt-[-77px] py-[26px] lg:px-20 md:px-[60px] sm:px-10 px-5 bg-bluish_purple z-40">
+    <nav
+      ref={catchScreen}
+      className="sm:fixed w-full relative top-[77px] flex justify-between items-center min-w-[250px] max-w-full mt-[-77px] py-[26px] lg:px-20 md:px-[60px] sm:px-10 px-5 bg-bluish_purple z-40"
+    >
+      {/* this is mobile navigation component**************************** */}
+      <div
+        className={`sm:hidden absolute top-0 right-0 z-50 bg-bluish_purple w-[250px] h-[100vh] duration-500 p-4
+      ${
+        mobileNavBtn
+          ? "translate-x-0 transition-transform"
+          : "translate-x-full transition-transform"
+      }`}
+      >
+        <div className="mobile_nav_content">
+          <ul className="flex flex-col gap-7 font-['Inter'] text-white text-sm font-medium">
+            <li>
+              <div
+                onClick={() => setmobileNavBtn(false)}
+                className="px-1 text-lg font-bold w-fit text-orange-400 border-2 border-orange-400 cursor-pointer"
+              >
+                X
+              </div>
+            </li>
+            <li>
+              <a href="#">SERVICES</a>
+            </li>
+            <li>
+              <a href="#">ABOUT US</a>
+            </li>
+            <li>
+              <a href="#">CONTACT US</a>
+            </li>
+            <li>
+              <a href="#">CAREERS</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* this is big screen navigation component**************************** */}
+
       <a href="#" className="relative w-[238.89px] h-[25px]">
         <img
           src={logo}
@@ -16,6 +75,7 @@ const Navbar = () => {
           src={menuIcon}
           className="w-[31px] h-[24px] sm:hidden block"
           alt="menuIcon.png"
+          onClick={() => setmobileNavBtn(true)}
         />
       </a>
 
@@ -24,7 +84,6 @@ const Navbar = () => {
           <a href="#">SERVICES</a>
         </li>
         <li>
-          {" "}
           <a href="#">ABOUT US</a>
         </li>
         <li>
